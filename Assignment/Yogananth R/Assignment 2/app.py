ca557e047115043cfbd6a9e44e4709b460a9dcb6
YOGANANTH R <yogiuthra22@gmail.com>
2022-09-29 19:24:20 +0530

    assignment 2 db changes

--- Assignment/Yogananth R/Assignment 2/app.py
+++ Assignment/Yogananth R/Assignment 2/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
+import ibm_db
+from markupsafe import escape
+
+conn=imb_db.connect("")
 
 app=Flask(__name__)
+
 
 @app.route("/")
 @app.route("/login",methods=['GET','POST'])
@@ -8,11 +13,21 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        us="yogi"
-        pw="password"
-        if username==us and password==pw:
-            return render_template("index.html",msg=us)
-    return render_template("home.html")
+
+        sqll= f"SELECT username, password from user_login where username= '{escape(username)}'"
+        stmt=ibm_db.exec_immediate(conn,sqll)
+        dictionary=ibm_db.fetch_assoc(stmt)
+        while dictionary != False:
+            db_username=dictionary["username"]
+            db_password=dictionary["password"]
+            dictionary=ibm_db.fetch_assoc(stmt)
+
+        
+        if username==db_username and password==db_password:
+            return render_template("index.html",msg=username)
+        else:
+            return render_template("login.html",msg="Login Invalid")
+    return render_template("login.html")
 
 @app.route("/register")
 def register():
