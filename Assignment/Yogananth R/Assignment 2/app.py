721329e001fe1c18206ca6395ddf1f2fbfe44a40
YOGANANTH R <yogiuthra22@gmail.com>
2022-09-29 20:39:26 +0530

    Assignment 2 db changes

--- Assignment/Yogananth R/Assignment 2/app.py
+++ Assignment/Yogananth R/Assignment 2/app.py
@@ -29,6 +29,27 @@ def login():
             return render_template("login.html",msg="Login Invalid")
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register",methods=['POST','GET'])
 def register():
+    if request.method=='POST':
+        username_reg=request.form['username']
+        password_reg=request.form['password']
+        email_reg=request.form['email']
+
+        sqlr="SELECT * from user_login WHERE username=?"
+        stmt=imb_db.prepare(conn,sqlr)
+        ibm_db.bind_param(stmt,1,username_reg)
+        ibm_db.execute(stmt)
+        account=ibm_db.fetch_assoc(stmt)
+
+        if account:
+            render_template('login.html',msg="You are already a member.")
+        else:
+            sqlreg="INSERT into user_login VALUES (?,?,?)"
+            prep_stmt=ibm_db.prepare(conn,sqlreg)
+            ibm_db.bind_param(prep_stmt,1,username_reg)
+            ibm_db.bind_param(prep_stmt,2,password_reg)
+            ibm_db.bind_param(prep_stmt,3,email_reg)
+            ibm_db.execute(prep_stmt)
+        return render_template("login.html",msg="Account Created. Login Now!!")
     return render_template("register.html")
